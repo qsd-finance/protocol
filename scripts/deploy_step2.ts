9a3053f427f23fc66e61996b8069510d21664453
demonicmnemonic <79536970+demonicmnemonic@users.noreply.github.com>
2021-03-18 20:08:41 -0400

    Add files via upload

--- scripts/deploy_step2.ts
+++ scripts/deploy_step2.ts
@@ -2,13 +2,20 @@ import { ethers } from "hardhat";
 const { provider } = ethers;
 const { parseEther } = ethers.utils;
 
-const dollarAddr = '0x96e3c8Fa6DE75712cca47B0db0Bd559aaa7Ac416';
-const governanceTokenAddr = '0x6F341b6Ff1a7A72A7E6c3fa1748afc2fb31E0d0A';
+/*
+{"claim":"0x50AA74434a536bf45eA82E9a460aD743dABDEE97",
+"dollar":"0xE8F12Efeb864b152AbBf6826338516746BB70820",
+"governanceToken":"0x97f721c18166eeb5Dee72873fd07bfD1b4D5F3A4"}
+*/
+
+
+const dollarAddr = '0xE8F12Efeb864b152AbBf6826338516746BB70820';
+const governanceTokenAddr = '0x97f721c18166eeb5Dee72873fd07bfD1b4D5F3A4';
 
 const treasuryAddr = '0x247C08e7f043B960457676516A3258484aD8e7Bb';
 const DAI = '0xe9e7cea3dedca5984780bafc599bd69add087d56'  //BUSD
 
-const uniV2PairAddress = 
+const uniV2PairAddress = '0x83ABc20d35Fc4C0ACF5d61f026107c94788373fA'
 
   const main = async () => {
     const owner = await provider.getSigner(0);
@@ -83,3 +90,16 @@ const uniV2PairAddress =
 
   main();
 
+/*
+
+{"dao":"0x0a28D6F9a63739176B36Eba95B0c95df360691E5",
+"poolBonding":"0x1Dcf6be37F1841aD5b986b2214F9E1e27D8e94d5",
+"poolLP":"0xB0819e5a5db7E82697F8549a53965a354a0871F9",
+"poolGov":"0x6c7041DE3a2B3Db37cfAe9DF167b95e7a02CcA00",
+"dollar":"0xE8F12Efeb864b152AbBf6826338516746BB70820",
+"governanceToken":"0x97f721c18166eeb5Dee72873fd07bfD1b4D5F3A4",
+"uniV2PairAddress":"0x83ABc20d35Fc4C0ACF5d61f026107c94788373fA",
+"implementation":"0x758fF0e49f5710392736A626815ff10915059eCf",
+"root":"0x0a28D6F9a63739176B36Eba95B0c95df360691E5"}
+
+*/
